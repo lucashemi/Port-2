b62c0ee4a4bc482e81c2c04bd87ec9c2b03eca0c
LucasBG <lucasxberger@gmail.com>
2022-03-15 12:06:10 -0300

    js pra voltar menu e css projetos largo

--- index.js
+++ index.js
@@ -4,3 +4,9 @@ const menu = document.querySelector('.menu-lateral')
 botaoMenu.addEventListener('click', () => {
     menu.classList.toggle('menu-lateral--ativo')
 })
+
+const main = document.querySelector('.container')
+
+main.addEventListener('click', () => {
+    menu.classList.remove('menu-lateral--ativo')
+})
